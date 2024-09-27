c1b3a530a5dab7f4d517e3587432be6ed8c298ba
Veronica Bravo <bravomv@hotmail.com>
2024-09-27 10:54:40 -0300

    delete para inicio de cero

--- src/regex/regex.js
+++
@@ -1,9 +0,0 @@
-export const REGEX = {
-	specialCharacters: /[¡!@#$%^&*(),.¿?":{}|<>-_+]/,
-	numbers: /[\d]/,
-	startWithSpace: /^\s/,
-	endWithSpace: /\s$/,
-	mayus: /[A-Z]/,
-	minus: /[a-z]/,
-	space: /\s/
-};
